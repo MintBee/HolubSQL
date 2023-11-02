0caa0fcdda7a755b8221897dbbc8695dbd88224a
cjdthsus2gh <cjdthsus2gh@naver.com>
2023-11-02 15:32:38 +0900

    implement Table.Exporter

--- src/com/holub/database/XMLExporter.java
+++ src/com/holub/database/XMLExporter.java
@@ -1,5 +1,21 @@
 package com.holub.database;
 
-public class XMLExporter {
+import java.io.*;
+import java.util.*;
 
+public class XMLExporter implements Table.Exporter {
+	private final Writer out;
+	private 	  int	 width;
+
+	public XMLExporter( Writer out ){
+		this.out = out;
+	}
+	public void storeMetadata( String tableName,int width,int height,Iterator columnNames ) throws IOException {
+
+	}
+	public void storeRow( Iterator data ) throws IOException {
+		
+	}
+	public void startTable() throws IOException {/*nothing to do*/}
+	public void endTable()   throws IOException {/*nothing to do*/}
 }
