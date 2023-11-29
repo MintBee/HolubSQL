9a618e772e27913038ec8321a015f7b5f1ab1c51
dkile <dkile532@naver.com>
2023-11-29 16:11:34 +0900

    InProgress: main test

---
+++ src/com/designpattern/Main.java
@@ -0,0 +1,17 @@
+package com.designpattern;
+
+import com.designpattern.io.InputBoundary;
+import com.designpattern.io.OutputBoundary;
+import com.designpattern.io.cli.CLIInput;
+import com.designpattern.io.cli.CLIOutputBoundary;
+
+public class Main {
+    public static void main(String[] args) {
+        OutputBoundary cliOutputBoundary = new CLIOutputBoundary();
+        Inventory inventory = new Inventory();
+        InputBoundary shop = new Shop(inventory, cliOutputBoundary);
+        CLIInput cli = new CLIInput(shop);
+
+        cli.start();
+    }
+}
